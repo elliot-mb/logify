e1f39be5c3e8aa5666b394571d6f57b827cc03dc
elliot-mb <elliotbuckingham14@gmail.com>
2023-08-31 22:46:52 +0100

    ignore auth

---
+++ front/src/routes/login/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { page } from "$app/stores";
+    import Content from "$lib/components/Content.svelte";
+    import Hero from "$lib/components/Hero.svelte";
+
+  const params: URLSearchParams = $page.url.searchParams;
+  const code: string | null = params.get('code'); 
+  const state: string | null = params.get('state');
+  const err: string | null = params.get('error'); //should be null else user rejected request 
+
+</script>
+
+<Hero/>
+<Content>
+  <h2>Redirected</h2>
+  <p>{code}</p>
+  <p>{state}</p>
+</Content>
