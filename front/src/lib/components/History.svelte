fda6d9495f260d4a17a2f0cd9b202356d633ebe2
elliot-mb <elliotbuckingham14@gmail.com>
2023-09-05 21:27:37 +0100

    refactored token fetch into server endpt

---
+++ front/src/lib/components/History.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+    import type { Spotify } from "../../spotify";
+
+  export let history: Spotify.PlayHistory[];
+
+</script>
+
+<div class="trackgrid">
+  <h3 class="cell">Name</h3>
+  <h3 class="cell">Artist(s)</h3>
+  <h3 class="cell">Album</h3>
+  <h3 class="cell">Played</h3>
+  {#if history !== null}
+    {#each history as h}
+      <span class="cell">{h.track.name}</span>
+      <span class="cell">{h.track.artists === null ? 'Unknown' : h.track.artists.join(', ')}</span>
+      <span class="cell">{h.track.album}</span>
+      <span class="cell">{h.played_at}</span>
+    {/each}
+  {/if}
+</div>
+
+<style>
+  .trackgrid{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    gap: 0rem;
+    background-color: var(--hero);
+    margin: 0 0 2rem 0;
+  }
+
+  .cell{
+    padding: 1rem;
+    border-width: 0.1rem;
+    border-style: solid none none none;
+    border-color: var(--bg);
+    margin: 0;
+  }
+</style>
--- front/src/routes/history/+page.svelte
+++ front/src/routes/history/+page.svelte
@@ -4,6 +4,7 @@
   import { Spotify } from '$lib/../spotify';
   import { token } from '$lib/../stores';
   import { onMount } from 'svelte';
+    import History from '$lib/components/History.svelte';
 
   export let data: PageData;
 
@@ -15,35 +16,8 @@
 
 <p>Filter your history and see see simple analytics by time period.</p>
 
-<div class="trackgrid">
-  <h3 class="cell">Name</h3>
-  <h3 class="cell">Artist(s)</h3>
-  <h3 class="cell">Album</h3>
-  <h3 class="cell">Played</h3>
-  {#if data.history !== null}
-    {#each data.history as h}
-      <span class="cell">{h.track.name}</span>
-      <span class="cell">{h.track.artists === null ? 'Unknown' : h.track.artists.join(', ')}</span>
-      <span class="cell">{h.track.album}</span>
-      <span class="cell">{h.played_at}</span>
-    {/each}
-  {/if}
-</div>
-
-<style>
-  .trackgrid{
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    gap: 0rem;
-    background-color: var(--hero);
-    margin: 0 0 2rem 0;
-  }
-
-  .cell{
-    padding: 1rem;
-    border-width: 0.1rem;
-    border-style: solid none none none;
-    border-color: var(--bg);
-    margin: 0;
-  }
-</style>
+{#if data.history === null}
+  <p>Please <a href="/login">login</a> to view your history</p>
+{:else}
+  <History history={data.history}/>
+{/if}
